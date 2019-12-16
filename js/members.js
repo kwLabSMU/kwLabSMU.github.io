e967add739767d23c90b2ee06cc8478d08f2cff4
clong <2956957982@qq.com>
2019-12-16 22:28:30 +0800

    初步完成历届成员

--- js/members.js
+++ js/members.js
@@ -13,38 +13,39 @@ Member.prototype = {
   el: function() {
     this.$el = {};
     this.$el.container = $(".jq-container");
-    this.$el.box = $("<div />").addClass("col-md-8");
-    this.$el.title = $("<h1 />")
-      .addClass("page-header sidebar-title")
-      .text("历届学生介绍");
-    this.$el.hr = $("<hr />");
+    this.$el.box = $("<div />");
+    this.$el.itemPic = $(".jq-student-img-item").detach();
+    this.$el.itemText = $(".jq-student-text").detach();
     this.$el.item = $(".jq-item")
       .detach()
       .show();
   },
   getData() {
-    return $.get("/data/json/members.json");
+    return $.get("/data/json/member.json");
   },
   render(data) {
     var that = this;
-    this.$el.box.append(this.$el.title);
     data.forEach(function(v) {
-      var item = $(that.$el.item).clone(),
-        hr = $(that.$el.hr).clone();
-      item
-        .find(".jq-name")
-        .text(v.name)
-        .end()
-        .find(".jq-time")
-        .text(v.time)
-        .end()
-        .find(".jq-location")
-        .text(v.location)
-        .end()
-        .find(".jq-introduction")
-        .text(v.introduction);
-      that.$el.box.append(item, hr);
+      var item = that.$el.item.clone();
+      item.find(".jq-student-time").text(v.year);
+      v.children.forEach(function(node) {
+        const picItem = that.$el.itemPic.clone();
+        picItem.find("img").attr("src", node.img);
+        const itemText = that.$el.itemText.clone();
+        itemText.html($("<strong />").text(node.name));
+        const locText = that.$el.itemText.clone().text(node.location);
+        item
+          .find(".jq-student-img")
+          .append(picItem)
+          .end()
+          .find(".jq-student-name")
+          .append(itemText)
+          .end()
+          .find(".jq-student-location")
+          .append(locText);
+      });
+      that.$el.box.append(item);
     });
-    this.$el.container.html(this.$el.box);
+    this.$el.container.append(that.$el.box);
   }
 };
